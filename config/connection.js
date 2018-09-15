e08920ef99442870369c144ca130b5e6c2051fc6
Jorge Garcia <jagarcia@ucdavis.edu>
2018-09-15 16:41:48 -0700

    update

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+var mysql = require("mysql");
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection({
+      port: 3300,
+      host: "localhost",
+      user: "root",
+      password: "password",
+      database: "burgers_db"
+    });
+  }
+
+connection.connect();
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO burgers (
+burger_name,
+devoured
+) VALUES
+(
+"Double Double Protein Style Burger",
+0
+),
+(
+"Avocado Cheese Melt Burger",
+0
+),
+(
+"Vegetarian Burger",
+0
+);
